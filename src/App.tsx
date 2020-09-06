88f6e62f6e816ee2b9c1b7fc6d689cc7e072f4e5
Arian Nargesi <ariannargesi@gmail.com>
2020-09-06 17:18:14 +0430

    generic changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
 import ButtonsGroup  from './Components/ButtonsGroup'
-
+import Cube from './Components/Cube'
 const Container = styled.div`
   width: 1200px;
   margin: 0 auto;
+`
+const CubeWrapperStyled = styled.div`
+  width: 200px;
+  height: 200px;
+  perspective: 600px;
 `
 
 export default function App() {
   return (
     <Container>
       <ButtonsGroup/>
+      <CubeWrapperStyled>
+        <Cube/>
+      </CubeWrapperStyled>
     </Container>
   )
 }
