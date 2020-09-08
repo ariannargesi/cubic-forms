de70af78b18bea3bedebf64b3b318f2b2304ba38
Arian Nargesi <ariannargesi@gmail.com>
2020-09-08 20:13:33 +0430

    general Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,15 @@
-import React from 'react'
-import styled from 'styled-components'
-import ButtonsGroup  from './Components/ButtonsGroup'
+import React from "react";
 import Cube from './Components/Cube'
-const Container = styled.div`
-  width: 1200px;
-  margin: 0 auto;
-`
-const CubeWrapperStyled = styled.div`
-  width: 200px;
-  height: 200px;
-  perspective: 600px;
-  position: absolute;
-  transform: scale(2);
-  top: 20%;
-  left: 30%;
-`
-
-export default function App() {
+import ButtonsGroup from './Components/ButtonsGroup'
+const App: React.FC = () => {
   return (
-    <Container>
-      <ButtonsGroup/>
-      <CubeWrapperStyled>
-        <Cube/>
-      </CubeWrapperStyled>
-    </Container>
-  )
-}
+    <>
+    <div style={{position: "absolute", left: "10%"}}>
+      <Cube/>
+    </div>
+    <ButtonsGroup/>
+    </>
+  );
+};
+
+export default App 
