973e982694e1c3cadf7311f89af6765d0fecd3cd
Arian Nargesi <ariannargesi@gmail.com>
2020-09-09 23:36:20 +0430

    default

--- src/Components/Signup/index.tsx
+++ src/Components/Signup/index.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import Input from '../Input'
+import React from "react";
+import Input from "../Input";
 const Signup: React.FC = () => {
-    return (
-        <div className="face face-front">
-            <Input/>
-        </div>
-    )
-}
+  return (
+    <div className="face face-front">
+      <h1>Singup</h1>
+      <form action="">
+        <Input placeholder="Email Address or phone number" />
+        <Input placeholder="Email Address" />
+        <Input placeholder="Password" />
+        <Input placeholder="Confirm Password" />
+        <Input type="submit"/>
+      </form>
+    </div>
+  );
+};
 
-export default Signup 
+export default Signup;
