e0f16e94f8e78144c47f33099b05123e63669e2f
Arian Nargesi <ariannargesi@gmail.com>
2020-09-11 23:21:55 +0430

    add Thanks

---
+++ src/Components/Thanks/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Input from '../Input'
+
+const Thanks: React.FC = () => {
+
+    return (
+        <div className="face face-bottom">
+          <h2>Hello </h2>
+        </div>
+    )
+}
+
+export default Thanks 
