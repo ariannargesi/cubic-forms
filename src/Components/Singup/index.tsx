e2d58e66a16c31d65d751587abe6f3709855af29
Arian Nargesi <ariannargesi@gmail.com>
2020-09-11 23:22:23 +0430

    add inputs

--- src/Components/Singup/index.tsx
+++ src/Components/Singup/index.tsx
@@ -1,27 +1,50 @@
-import React from 'react'
-import Input from '../Input'
-
+import React, { useState } from "react";
+import GoogleAuth from '../GoogleAuth'
+import Input from "../Input";
 
 const Signup: React.FC = () => {
-    return (
-        <div className="face face-right">
-            <div className='d-flex'>
-                <h1>Sing up</h1>
-                <a href="#">Log in</a>
-            </div>
-            <label>Email address</label>
-            <Input type="eamil" placeholder="example@emial.com"/>
-            <label>Password</label>
-            <Input type="password"/>
-            <label>Confirm password</label>
-            <Input type="password"/>
-            <div style={{display: 'flex', alignItems: 'center'}}>
-                <Input type="checkbox" id="terms"/>
-                <label className="font-small ml-1 pointer" htmlFor="terms">i agree with the <a href="#">terms of service</a></label>
-            </div>
-            <Input type="submit"/>
+  const [emailValue, setEmailValue] = useState<string>("");
+  const [passwordValue, setPasswordValue] = useState<string>("");
+  const [confirmValue, setConfirmValue] = useState<string>("");
+  const [ termValue, setTermValue ] = useState<boolean>(false)
+  return (
+    <div className="face face-right">
+      <form action="">
+        <div className="d-flex">
+          <h1>Sing up</h1>
+          <a href="#">Log in</a>
         </div>
-    )
-}
+        <label>Email address</label>
+        <Input
+          type="eamil"
+          value={emailValue}
+          onChange={(e)=> setEmailValue(e.target.value)}
+          placeholder="example@emial.com"
+        />
+        <label>Password</label>
+        <Input
+            type="password" 
+            value={passwordValue}
+            onChange={e => setPasswordValue(e.target.value)}
+        />
+        <label>Confirm password</label>
+        <Input
+            type="password" 
+            value={confirmValue}
+            onChange={ e => setConfirmValue(e.target.value) }
 
-export default Signup 
+        />
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <Input type="checkbox" id="terms" checked={termValue} onChange={() => setTermValue(!termValue)} />
+          <label className="font-small ml-1 pointer" htmlFor="terms">
+            i agree with the <a href="#">terms of service</a>
+          </label>
+        </div>
+        <Input type="submit"/>
+        <button>Google Auth</button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
