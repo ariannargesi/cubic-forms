b6150ae359dee5f7a28b0a2a2031cabbe399fc6e
Arian Nargesi <ariannargesi@gmail.com>
2020-09-11 23:24:32 +0430

    move styles to css files

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -1,25 +1,11 @@
 import React from 'react'
 import Styled from 'styled-components'
-
-const ButtonStyled = Styled.button`
-    border: none;
-    display: block;
-    padding: 0.6rem;
-    color: white;
-    background-color: blue;
-    border-radius: 20px;
-    font-size: 16px;
-    text-align: right;
-    margin: 1rem;
-    outline: none;
-    cursor: pointer;
-    transition-duration: 0.4s;
-`
+import './Button.css'
 type ButtonProps = React.ComponentPropsWithoutRef<'button'>
 
 const Button: React.FC<ButtonProps> = (props) => {
     return (
-    <ButtonStyled  {...props} >{props.children}</ButtonStyled>
+    <button className="button"  {...props} >{props.children}</button>
     )   
 }
 
