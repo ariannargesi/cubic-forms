6a33ef339d9149384122d476151168cd529b1c0a
Arian Nargesi <ariannargesi@gmail.com>
2020-09-11 23:24:14 +0430

    googleAuth

---
+++ src/Components/GoogleAuth.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const GoogleAuth: React.FC = () => {
+    React.useEffect(() => {
+        // window.gapi.load('client:auth2 ')
+    }, [])
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default GoogleAuth 
