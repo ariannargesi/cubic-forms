4535ed04797d3a432d7d1caba721f3daf052628a
Arian Nargesi <ariannargesi@gmail.com>
2020-09-13 19:55:15 +0430

    delete styled-components style

--- src/Components/ButtonsGroup/index.tsx
+++ src/Components/ButtonsGroup/index.tsx
@@ -1,21 +1,11 @@
 import React from 'react'
-import styled from 'styled-components'
 import {Store, ActionTypes} from '../../Context'
 import Button from '../Button'
 
-const ButtonsGroupStyled = styled.div`
-    direction: rtl;
-    position: absolute;
-    top: 30%;
-    right: 5%;
-`
-// 308560492398-kjesqudoqndn8ca1611ft11qlq88um4s.apps.googleusercontent.com
+import './ButtonsGroup.css'
 
 const ButtonsGroup: React.FC = () => {
-
-    // or [state, dispatch] 
     const store = React.useContext(Store)
-    
 
     const dispatcher = (value: string): void => {
         store.dispatch({
@@ -24,14 +14,14 @@ const ButtonsGroup: React.FC = () => {
         })
     }
     return (
-        <ButtonsGroupStyled>
+        <div className="buttons-group">
             <Button onClick= { () => dispatcher('show-front') }>Login</Button>
             <Button onClick= { () => dispatcher('show-top') }> Sign up </Button>
             <Button onClick= { () => dispatcher('show-back') }> Forget password </Button>
             <Button onClick= { () => dispatcher('show-right') }> Subscribe </Button>
             <Button onClick= { () => dispatcher('show-left') }> Contact us </Button>
             <Button onClick= { () => dispatcher('show-bottom') }> Message </Button>
-        </ButtonsGroupStyled>
+        </div>
         
     )
 }
