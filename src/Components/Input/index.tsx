b4037d90d2a752df6fd42789a06c76000e190b8d
Arian Nargesi <ariannargesi@gmail.com>
2020-09-13 19:57:14 +0430

    add wrapper for the inputs

--- src/Components/Input/index.tsx
+++ src/Components/Input/index.tsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React from "react";
+import "./Input.css";
 
-import './Input.css'
+type InputProps = React.ComponentPropsWithoutRef<"input">;
 
-type InputProps = React.ComponentPropsWithoutRef<'input'>
+type Validation = {
+  validation?: {
+    error: string;
+    showError: boolean;
+  };
+};
 
-const Input: React.FC <InputProps> =(props) =>{
-    React.useEffect(() => {
-        console.log("re-render")
-    })
-    return <input 
-                className="input"
-                {...props}/>
-}
+const Input: React.FC<InputProps & Validation> = (props) => {
+  const [isActive, toggleActive] = React.useState(false);
+  return (
+    <div className={"input-wrapper"}>
+      <span className="error-wrapper">
+        {props.validation?.showError && isActive && (
+          <h5>{props.validation?.error}</h5>
+        )}
+      </span>
+      <input className="input" {...props} onBlur={() => toggleActive(true)} />
+    </div>
+  );
+};
 
-
-export default Input
+export default Input;
