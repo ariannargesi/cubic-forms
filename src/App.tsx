47f79ad9e50d340819e66f35c7350af0d4bc59f4
Arian Nargesi <ariannargesi@gmail.com>
2020-09-14 13:14:12 +0430

    add CubeContextProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import CubeForms from './Components/CubeForms'
 import  CubeHints from './Components/CubeHints'
 import ButtonsGroup from './Components/ButtonsGroup'
+import CubeContextProvider from './Context/CubeContext'
 const App: React.FC = () => {
   return (
     <>
     <ButtonsGroup/>
     <div className="cubes">
-      <CubeForms/>
-      <CubeHints/>
+    <CubeContextProvider>
+        <CubeForms/>
+        <CubeHints/>
+     </CubeContextProvider>
     </div>
     
     <div className="anim-circle"></div>
