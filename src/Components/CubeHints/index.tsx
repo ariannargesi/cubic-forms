0227317753ac937cf136e18cb60d87925a1a98b4
Arian Nargesi <ariannargesi@gmail.com>
2020-09-14 13:13:30 +0430

    change the faecs

--- src/Components/CubeHints/index.tsx
+++ src/Components/CubeHints/index.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+
+import SingupHints from './Singup_hint'
+
 import { Store } from "../../Context";
+
+
 import "./CubeHints.css";
 
 const CubeHints: React.FC = () => {
@@ -10,11 +15,11 @@ const CubeHints: React.FC = () => {
   return (
     <div className="wrapper cube-hints">
       <div className={`rec-prism ${currentFace}`}>
+        <SingupHints/>        
         <div className="face face-back"></div>
         <div className="face face-left"></div>
-        <div className="face face-front"></div>
         <div className="face face-top"></div>
-        <div className="face face-right"></div>
+        <div className="face face-front"></div>
         <div className="face face-bottom"></div>
       </div>
     </div>
