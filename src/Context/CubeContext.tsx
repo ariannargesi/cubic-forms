a26611c45df8a046eccd90ca0fb5ebeff2a82a3b
Arian Nargesi <ariannargesi@gmail.com>
2020-09-16 23:18:59 +0430

    add

--- src/Context/CubeContext.tsx
+++ src/Context/CubeContext.tsx
@@ -4,63 +4,56 @@ export const Store = React.createContext<any>({})
 
 interface IAction {
     type: string,
-    payload: number 
+    payload: any  
 }
 
-enum StateEnum {
+export enum StateEnum {
+    Success,
+    Error,
+    Warnning, 
     Is_Typing,
     Completed,
     Empty,
-    Success,
-    Error
 }
 
-type IHintProps = {
+export type HintType = {
+    title: string 
     isActive: boolean,
     value: string,
-    state: StateEnum     
 }
 
 interface IinitialState {
-    signup: [IHintProps,IHintProps,IHintProps ]
+    signup: [HintType,HintType,HintType ]
 }
 
 
 
 const initialState: IinitialState = {
     signup: [
+        {   title: "Email Address",
+            isActive: false,
+            value: "",
+        },
+        {
+            title: "Password",
+            isActive: false,
+            value: "",
+        },
         {   
+            title: "Confirm Password",
             isActive: false,
             value: "",
-            state: StateEnum.Empty
-        },
-        {
-            isActive: false,
-            value: "",
-            state: StateEnum.Empty
-        },
-        {
-            isActive: false,
-            value: "",
-            state: StateEnum.Empty
         } 
     ]
 }
 
-enum ActionTypes {
-    toggleSignupInput= "0",
-    updateSignupInputsValue = "1"
-
-}
-
 const reducer = (state: IinitialState, action: IAction): IinitialState => {
-    switch(action.type){
-        case ActionTypes.toggleSignupInput: 
-            const newState = [ ...state.signup ]
-             
-        default: 
-            return state  
-    }
+    const newState = {...state } 
+    if(action.type === "TOGGLE_SIGNUP_ITEM_FOCUS")
+         newState.signup[action.payload].isActive  = !newState.signup[action.payload].isActive
+    else if (action.type === "UPDATE_SIGNUP_INPUT_VALUE") 
+         newState.signup[action.payload.index].value = action.payload.value  
+    return newState  
 }
 
 const CubeContextProvider: React.FC = (props: any) =>{
