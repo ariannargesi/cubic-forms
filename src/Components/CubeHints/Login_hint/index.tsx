297fdf467bf4b3930ddcd4ae3c028011db565aa4
Arian Nargesi <ariannargesi@gmail.com>
2020-09-18 02:35:58 +0430

    add login

--- src/Components/CubeHints/Login_hint/index.tsx
+++ src/Components/CubeHints/Login_hint/index.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { isEmail, isValidPassword } from '../../../validation'
+import { Store } from '../../../Context/CubeContext'
 import Hint from '../HintsItem'
 
 const LoginHint: React.FC = () => {
+
+    const { login } = React.useContext(Store).state 
+    const [ email, password ] = login 
+
     return (
         <div className="face face-front">
             <h1>Login Hint</h1>
+            <div className="hints-wrapper">
+                <Hint
+                    isValid={isEmail(email.value.trim())}
+                    { ...email }
+                />
+                <Hint
+                    isValid={isValidPassword(password.value)}
+                    { ...password }
+                />
+            </div>
         </div>
     )
 }
