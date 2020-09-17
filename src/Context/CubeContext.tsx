82f7c5e4a127ac158fedaf7757b89527d7cb007c
Arian Nargesi <ariannargesi@gmail.com>
2020-09-17 18:49:41 +0430

    a lots of changes

--- src/Context/CubeContext.tsx
+++ src/Context/CubeContext.tsx
@@ -4,62 +4,77 @@ export const Store = React.createContext<any>({})
 
 interface IAction {
     type: string,
-    payload: any  
+    payload: any
 }
 
 export enum StateEnum {
     Success,
     Error,
-    Warnning, 
+    Warnning,
     Is_Typing,
     Completed,
     Empty,
 }
 
 export type HintType = {
-    title: string 
-    isActive: boolean,
+    title: string
+    isActive: boolean | null,
     value: string,
 }
-
+type PressButton = {
+    counter: number,
+    isTyping: boolean
+}
 interface IinitialState {
-    signup: [HintType,HintType,HintType ]
+    typing: PressButton, 
+    signup: [HintType, HintType, HintType]
 }
 
 
 
-const initialState: IinitialState = {
+const initialState: IinitialState = { 
+    typing: {
+        counter: 0,
+        isTyping: false 
+    },
     signup: [
-        {   title: "Email Address",
-            isActive: false,
+        {
+            title: "Email Address",
+            isActive: null,
             value: "",
         },
         {
             title: "Password",
-            isActive: false,
+            isActive: null,
             value: "",
         },
-        {   
+        {
             title: "Confirm Password",
-            isActive: false,
+            isActive: null,
             value: "",
-        } 
+        }
     ]
 }
 
 const reducer = (state: IinitialState, action: IAction): IinitialState => {
-    const newState = {...state } 
-    if(action.type === "TOGGLE_SIGNUP_ITEM_FOCUS")
-         newState.signup[action.payload].isActive  = !newState.signup[action.payload].isActive
-    else if (action.type === "UPDATE_SIGNUP_INPUT_VALUE") 
-         newState.signup[action.payload.index].value = action.payload.value  
-    return newState  
+    const newState = { ...state }
+    if (action.type === "TOGGLE_SIGNUP_ITEM_FOCUS")
+        newState.signup[action.payload].isActive = !newState.signup[action.payload].isActive
+    else if (action.type === "UPDATE_SIGNUP_INPUT_VALUE")
+        newState.signup[action.payload.index].value = action.payload.value 
+    else if(action.type === "UPDATE_COUNTER") 
+        newState.typing.counter += 10       
+    else if(action.type === "RESET_COUNTER")
+        newState.typing.counter = 0
+    else if(action.type === "TOGGLE_IS_TYPING")
+        newState.typing.isTyping =  action.payload  
+    return newState
 }
 
-const CubeContextProvider: React.FC = (props: any) =>{
-    const [ state, dispatch ] = React.useReducer(reducer, initialState)
+const CubeContextProvider: React.FC = (props: any) => {
+    const [state, dispatch] = React.useReducer(reducer, initialState)
     return (
-        <Store.Provider value={{state, dispatch}}>
+        <Store.Provider value={{ state, dispatch }}>
             {props.children}
         </Store.Provider>
     )
