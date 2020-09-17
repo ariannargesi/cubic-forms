af6be2fa099ce79247e84d14cc895690418ba287
Arian Nargesi <ariannargesi@gmail.com>
2020-09-17 18:49:55 +0430

    add typing effect

---
+++ src/Components/IsTyping/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './IsTyping.css'
+
+const IsTyping: React.FC = ({ children }) => {
+    return (
+        <div>
+            <span>{children}</span>
+            <span style={{marginLeft: "10px"}}>
+                <span className="typing-animation delay-1"></span>
+                <span className="typing-animation delay-2"></span>
+                <span className="typing-animation "></span>
+            </span>
+        </div>
+    )
+}
+
+export default IsTyping 
