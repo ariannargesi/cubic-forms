b9b8944e2c10654682b40a58df41a01e0d4f3c9b
Arian Nargesi <ariannargesi@gmail.com>
2020-09-18 02:36:25 +0430

    make it dynamic

--- src/Components/CubeForms/Login/index.tsx
+++ src/Components/CubeForms/Login/index.tsx
@@ -1,30 +1,48 @@
-import React, { FormEvent } from "react";
+import React from "react";
 import Input from "../../Input";
 
+import { Store, FormType } from "../../../Context/CubeContext";
 
-import './Login.css'
+import "./Login.css";
 
 const Signup: React.FC = () => {
-  const [nameValue, setNameValue] = React.useState<string>("");
-  const [emailValue, setEmailValue] = React.useState<string>("");
-  const [passwordValue, setPasswordValue] = React.useState<string>("");
-  const [confirmValue, setConfirmValue] = React.useState<string>("");
+  const { state, toggleFocus, updateInput } = React.useContext(Store);
+
+  const { login } = state 
+
+  const [ email, password ] = login 
 
 
   return (
     <div className="face face-front">
       <h1>Login</h1>
       <label>Email Address</label>
-      <Input placeholder="example@email.com"/>
+      <Input
+        type="email"
+        value={email.value}
+        onChange={(e) => updateInput(0, e.target.value, FormType.Login)}
+        onFocus={() => toggleFocus(0, FormType.Login)}
+        onBlurCapture={() => toggleFocus(0, FormType.Login)}
+        placeholder="example@email.com"
+      />
       <label> Password</label>
-      <Input type="password"/>
-      <div style={{display: 'flex', alignItems: 'center'}}>
-          <Input type="checkbox" id="remember"/>
-          <label className="font-small ml-1 pointer" htmlFor="remember"> remember me</label>
+      <Input 
+        type="password"
+        value={password.value}
+        onChange = { e => updateInput(1, e.target.value, FormType.Login) }
+        onFocus={ () => toggleFocus(1, FormType.Login) }
+        onBlurCapture={ () => toggleFocus(1,FormType.Login) }
+       />
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <Input type="checkbox" id="remember" />
+        <label className="font-small ml-1 pointer" htmlFor="remember">
+          {" "}
+          remember me
+        </label>
       </div>
-      <Input type="submit"/>
+      <Input type="submit" />
     </div>
-  )
-}
+  );
+};
 
 export default Signup;
