fe503f229e5f9860e22e5970302e885d57450395
Arian Nargesi <ariannargesi@gmail.com>
2020-09-17 18:50:53 +0430

    real shit it's here

--- src/Components/CubeForms/index.tsx
+++ src/Components/CubeForms/index.tsx
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Login from './Login'
 import Signup from './Singup'
 import ForgetPassword from './ForgetPassword'
 import Thanks from './Thanks'
-import { Store } from "../../Context";
+import { Store } from "../../Context";  
+import { Store as CubeStore } from '../../Context/CubeContext'
 import Input from '../Input'
 import "./Cube.css";
 
 const Cube: React.FC = () => {
+
+  const { state, dispatch } = React.useContext(CubeStore) 
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      dispatch({ type: "UPDATE_COUNTER" })
+    }, 50)
+    return () => clearInterval(intervalId)
+  },[])
+
+  // Detect typing here here
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const counter = state.typing.counter
+
+      let isTyping 
+
+      if(counter < 50) isTyping = true 
+      else isTyping = false 
+      
+      const action = {
+        type: "TOGGLE_IS_TYPING",
+        payload: isTyping 
+      }
+      
+      dispatch(action)
+
+    }, 50)
+
+    return () => clearInterval(intervalId)
+  }, [])
+
   const currentFace = React.useContext(Store).state.currentFace;
   return (
     <div className="wrapper">
