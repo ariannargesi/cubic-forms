88896f549509796eccfd7d5c47f57cbbb1756e8b
Arian Nargesi <ariannargesi@gmail.com>
2020-09-19 21:54:58 +0430

    thanks component

---
+++ src/Components/CubeHints/Thanks_hint/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+const ThanksHint: React.FC = () => {
+    return (
+        <div className="face face-bottom">
+            <div>
+                <p style={{textAlign: 'center', marginTop: '9rem'}}>Hints are empty</p>
+            </div>
+        </div>
+    )
+}
+
+export default ThanksHint
