905b1d59f38de1de8470239009369994ba2a66e2
Arian Nargesi <ariannargesi@gmail.com>
2020-09-19 21:58:07 +0430

    forgetPassword component

--- src/Components/CubeForms/ForgetPassword/index.tsx
+++ src/Components/CubeForms/ForgetPassword/index.tsx
@@ -1,27 +1,47 @@
 import React from 'react'
 import Input from '../../Input'
+import Hint from '../../CubeHints'
+import { Store, FormType } from '../../../Context/CubeContext'
+import { isValidPassword } from '../../../validation'
+import { Store as FaceStore, ActionTypes } from '../../../Context'
 import './ForgetPassword.css'
 
 
 const ForgetPassword: React.FC = () => {
 
-    const [value, setValue] = React.useState("")
+    const { state, toggleFocus, updateInput } = React.useContext(Store)
 
-    const isValid = (val: string) => {
-        if(val.length < 5)
-            return true 
-         return false 
+    const { dispatch } = React.useContext(FaceStore)
+
+    const { forgetPassword } = state
+
+    const [password] = forgetPassword
+
+    const submitIsDisable = !(isValidPassword(password.value))
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+        e.preventDefault()
+        dispatch({
+            type: ActionTypes.UpdateFace,
+            payload: 'show-bottom'
+        })
     }
 
     return (
         <div className="face face-top">
-            <Input
-                validation={ {
-                    showError: isValid(value),
-                    error: 'This is Error Message'
-                } } 
-                value={value}
-                onChange={ e => setValue(e.target.value) } />
+            <h1>Forget Password</h1>
+            <p> We will sent you and email that contain a link that you can reset your password with it </p>
+            <form  onSubmit={(e) => handleSubmit(e)}>
+                <Input
+                    type="email"
+                    placeholder="example@email.com"
+                    value={forgetPassword[0].value}
+                    onChange={e => updateInput(0, e.target.value, FormType.ForgetPassword)}
+                    onFocus={() => toggleFocus(0, FormType.ForgetPassword)}
+                    onBlurCapture={() => toggleFocus(0, FormType.ForgetPassword)}
+                />
+                <Input type="submit" style={{ marginTop: '2rem' }} disabled={submitIsDisable} />
+            </form>
         </div>
     )
 }
