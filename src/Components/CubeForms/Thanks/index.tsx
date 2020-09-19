635e52b0fb4a4ba79053b93d6e1dd97ca02b0cd3
Arian Nargesi <ariannargesi@gmail.com>
2020-09-19 21:56:59 +0430

    content changed

--- src/Components/CubeForms/Thanks/index.tsx
+++ src/Components/CubeForms/Thanks/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Input from '../../Input'
+import checkIcon from '../../../assets/check.png'
 
 const Thanks: React.FC = () => {
 
     return (
         <div className="face face-bottom">
-          <h2>Hello </h2>
+            <h1>Thank you</h1>
+            <p>please consider that this application is jut a sample and there is no backend for responding to request </p>
         </div>
     )
 }
