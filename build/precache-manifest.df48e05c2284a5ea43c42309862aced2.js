680c29b2f04e363246bf81c2ede558bf77de6677
Arian Nargesi <ariannargesi@gmail.com>
2020-09-20 16:15:15 +0430

    build folder

---
+++ build/precache-manifest.df48e05c2284a5ea43c42309862aced2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "647ce6146ee519ce1e01005f95a964a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43e67901c7a5e6c960fd",
+    "url": "/static/css/main.82d386f1.chunk.css"
+  },
+  {
+    "revision": "4fff37f68dfa40a0e55a",
+    "url": "/static/js/2.be51dd02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.be51dd02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43e67901c7a5e6c960fd",
+    "url": "/static/js/main.cefd176d.chunk.js"
+  },
+  {
+    "revision": "3938764256ee374ab697",
+    "url": "/static/js/runtime-main.7055714b.js"
+  },
+  {
+    "revision": "ea7e03e4dcd6d6afd947cc477ac1a501",
+    "url": "/static/media/gears.ea7e03e4.gif"
+  }
+]);
