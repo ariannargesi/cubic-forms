eea8e8fa4e3ad4189deb1568e51e2440b5186cc8
Arian Nargesi <ariannargesi@gmail.com>
2020-09-23 17:06:39 +0330

    remove wrapper

--- src/Components/Input/index.tsx
+++ src/Components/Input/index.tsx
@@ -3,26 +3,9 @@ import "./Input.css";
 
 type InputProps = React.ComponentPropsWithoutRef<"input">;
 
-type Validation = {
-  validation?: {
-    error: string;
-    showError: boolean;
-  };
-};
-
-const Input: React.FC<InputProps & Validation> = (props) => {
-
-  const [showHint, setShowHint] = React.useState(false);
-
+const Input: React.FC<InputProps> = (props) => {
   return (
-    <div className={"input-wrapper"}>
-      <span className="error-wrapper">
-        {props.validation?.showError && showHint && (
-          <h5>{props.validation?.error}</h5>
-        )}
-      </span>
-      <input className="input" {...props} onBlur={() => setShowHint(true)} />
-    </div>
+      <input className="input" {...props}  />
   );
 };
 
