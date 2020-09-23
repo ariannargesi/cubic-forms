3eb1a370f046feab4a81057ff26c894477042aae
Arian Nargesi <ariannargesi@gmail.com>
2020-09-23 17:07:08 +0330

    optional rendering for children span

--- src/Components/IsTyping/index.tsx
+++ src/Components/IsTyping/index.tsx
@@ -4,7 +4,7 @@ import './IsTyping.css'
 const IsTyping: React.FC = ({ children }) => {
     return (
         <div>
-            <span>{children}</span>
+            { children && <span>children</span> }
             <span style={{marginLeft: "10px"}}>
                 <span className="typing-animation delay-1"></span>
                 <span className="typing-animation delay-2"></span>
