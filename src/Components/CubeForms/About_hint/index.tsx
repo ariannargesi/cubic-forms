18b1b8a221e2cf9a825e0f5de83987b5a12a8a21
Arian Nargesi <ariannargesi@gmail.com>
2020-09-28 01:45:15 +0330

    about hint

---
+++ src/Components/CubeForms/About_hint/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './About_hint.css'
+
+const About_Hint: React.FC = () => {
+    return (
+        <div className="face face-back">
+            
+        </div>
+    )
+}
+
+export default About_Hint
