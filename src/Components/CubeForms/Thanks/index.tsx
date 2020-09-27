6edc7b30cfaa04e35ff1eb2c86de80aa1fb55983
Arian Nargesi <ariannargesi@gmail.com>
2020-09-28 01:43:53 +0330

    thanks component added

--- src/Components/CubeForms/Thanks/index.tsx
+++ src/Components/CubeForms/Thanks/index.tsx
@@ -7,7 +7,28 @@ const Thanks: React.FC = () => {
     return (
         <div className="face face-bottom">
             <h1>Thank you</h1>
-            <p>please consider that this application is jut a sample and there is no backend for responding to request </p>
+            <p style={{color: "white"}}>
+                Thanks you for checking out.
+            </p>
+            <p style={{color: 'white'}}>
+            Please consider this project it's only for testing cubic shapes on the web, and there is no server for authentication the users.
+            </p>
+            <div>
+                <a href='https://github.com/ariannargesi/cubic-forms'>Github</a>
+            </div>
+            <p style={{color: "white"}}>
+            check out my other projects too:
+                <br/>
+                <a href="https://github.com/ariannargesi/stickershop">
+                    Sticker Shop
+                </a>
+                <br/>
+                <a href="https://github.com/ariannargesi/scream-master">
+                    Scream Master
+                </a>
+            </p>
+            
+
         </div>
     )
 }
